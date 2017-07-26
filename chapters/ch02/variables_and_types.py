f23d4499f44af00fba936bb40713065c1375152e
Sushil Verma <sushil@vermanotes.com>
2017-07-26 14:06:12 +0530

    Python variables and Data types examples.

---
+++ chapters/ch02/variables_and_types.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+# variables in Python
+
+height = 1.79
+weight = 68.7
+bmi = weight / height ** 2
+
+# printing value of bmi
+print(bmi)
+
+# check type of a variable
+
+print('Type of bmi variable is: ', type(bmi))
+
+day_of_week = 5
+print('Type of day_of_week variable is: ', type(day_of_week))
+
+x = "Body mass index"
+y = 'this works too'
+
+print('Type of x variable is: ', type(x))
+print('Type of y variable is: ', type(y))
+
+z = True
+print('Type of z variable is: ', type(z))
+
+sum_two_integers = 2 + 3
+print('Value of sum_two_integers is: ', sum_two_integers)
+
+sum_two_strings = 'ab' + 'cd'
+print('Value of sum_two_strings is: ', sum_two_strings)
